831bdd9fb755a88871408415eb0301839d9ce097
Nick Peirson <nickpeirson@gmail.com>
2020-10-17 15:45:29 +0100

    Initial Commit

---
+++ MidiMapper/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  MidiMapper
+//
+//  Created by Nick Peirson on 17/10/2020.
+//  Copyright © 2020 Nick Peirson. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
