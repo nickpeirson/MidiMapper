6f368b4af52f8df5c6714aff3b86e6618fe24bce
Nick Peirson <nickpeirson@gmail.com>
2024-12-20 09:49:53 +0000

    Implement brightness adjustment in HueAPI with NSURLSession for API calls

--- MidiMapper/HueAPI.m
+++ MidiMapper/HueAPI.m
@@ -1,6 +1,9 @@
 #import "HueAPI.h"
 #import "HueBridgeDiscovery.h"
 
+@interface HueAPI () <NSURLSessionDelegate>
+@end
+
 @implementation HueAPI {
     HueBridgeDiscovery *bridgeDiscovery;
 }
@@ -16,8 +19,56 @@
 }
 
 - (void)setBrightness:(NSInteger)brightness {
-    // Stubbed method: Implement the actual API call to set brightness
-    NSLog(@"Setting brightness to %ld", (long)brightness);
+    NSString *bridgeIP = [self getBridgeIPAddress];
+    if (!bridgeIP) {
+        NSLog(@"Bridge IP address not found.");
+        return;
+    }
+
+    NSString *urlString = [NSString stringWithFormat:@"https://%@/clip/v2/resource/grouped_light/1d70a073-d47a-4e02-931c-a35db2a8bf1e", bridgeIP];
+    NSURL *url = [NSURL URLWithString:urlString];
+    NSMutableURLRequest *request = [NSMutableURLRequest requestWithURL:url];
+    request.HTTPMethod = @"PUT";
+
+    // Set the header with the application key
+    NSString *applicationKey = @"hBJ7J1M4esLnNC5yfyjLPqHygpyXgmU6vNLsHXQG";
+    [request setValue:applicationKey forHTTPHeaderField:@"hue-application-key"];
+
+    // Set the JSON body
+    NSDictionary *bodyDict = @{@"dimming": @{@"brightness": @(brightness)}};
+    NSError *error;
+    NSData *bodyData = [NSJSONSerialization dataWithJSONObject:bodyDict options:0 error:&error];
+    if (!bodyData) {
+        NSLog(@"Error serializing JSON: %@", error.localizedDescription);
+        return;
+    }
+    request.HTTPBody = bodyData;
+
+    // Create a session configuration
+    NSURLSessionConfiguration *sessionConfig = [NSURLSessionConfiguration defaultSessionConfiguration];
+
+    // Create the session with the delegate set to self
+    NSURLSession *session = [NSURLSession sessionWithConfiguration:sessionConfig delegate:self delegateQueue:nil];
+
+    // Create the data task
+    NSURLSessionDataTask *task = [session dataTaskWithRequest:request
+                                            completionHandler:^(NSData *data, NSURLResponse *response, NSError *error) {
+        if (error) {
+            NSLog(@"Error setting brightness: %@", error.localizedDescription);
+        } else {
+            NSLog(@"Brightness set to %ld", (long)brightness);
+        }
+    }];
+
+    [task resume];
+}
+
+// Implement NSURLSessionDelegate method to ignore SSL errors
+- (void)URLSession:(NSURLSession *)session didReceiveChallenge:(NSURLAuthenticationChallenge *)challenge
+ completionHandler:(void (^)(NSURLSessionAuthChallengeDisposition disposition, NSURLCredential *credential))completionHandler
+{
+    NSURLCredential *credential = [[NSURLCredential alloc] initWithTrust:challenge.protectionSpace.serverTrust];
+    completionHandler(NSURLSessionAuthChallengeUseCredential, credential);
 }
 
 - (NSString *)getBridgeIPAddress {
